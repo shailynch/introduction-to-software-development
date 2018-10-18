efd6cbd4dced46401251e53a16e2be4e246f35d8
shailynch <shai.lynch.sl@gmail.com>
2018-10-18 21:26:46 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,118 @@
+print ("Week 2 task 3")
+
+print ("hello world")
+
+print ("Week 2 task 4")
+
+print ("hello world")
+
+print ("I am in my ISD class right now")
+
+print ("Hi there, what is your name? ")
+
+userName = input("Type your name")
+
+print ("Hell0 ", userName)
+
+print (userName)
+
+print ("Week 3 task 1")
+
+print("enter 2 number")
+num1 = int(input())
+num2 = int(input())
+print(num1 + num2)
+
+print ("week 3 task 2 a")
+
+num = 4
+num*=2
+num1=num+2
+num1+=3
+print(num1)
+
+print ("week 3 task 2 b")
+
+print(2 / 3)
+print(2 // 3)
+
+print ("Week 4 task 1")
+
+radius_t1 = input("radius")
+
+x = 3.14
+pi = x
+area = pi * int(radius_t1) **2
+
+#must use the int method to get the int version
+print(area)
+
+print ("Week 4 task 2")
+
+
+x = 4
+y = 5
+a = 3 * (x + y)
+# short hand maths such as 3(x +y) wont work full equations only
+
+print(a)
+
+print ("Week 4 task 3")
+
+radius_t3 = float(input("Enter the radius:"))
+#can only float an input variable float inside an input wont workas it doesnt pull the value as the value in this case is letters
+
+
+print (radius_t3)
+
+print ("Week 4 task 4")
+
+print ((3+4+5)/ 3)
+#due to bidmas bodmas and pemdas the division will be calculated first and has to be removed from brackets
+
+print ("Week 4 task 5")
+
+x = 19.93
+y = 20
+z = y - x
+
+#the round function can round to a specified number of decimal points
+print (round(z,2))
+
+print ("Week 4 task 6")
+x =2
+print (x**2)
+print (x**3)
+
+#using built in syntax reduces chqnce of compile time errors
+#compile time errors are a type of error that prevents a program being compiled
+
+print ("Week 4 task 7")
+x = 2
+y = 4
+print("The product of",	x, "and", y, "is", x + y)
+print("The root	of their difference is	", sqrt(x – y))
+
+print ("Week 4 task 8")
+x = input("enter your first name here ")
+y = int(input("print your age here "))
+print ("Hello", x, "next year you will be", y+1, "years old!")
+
+print ("Week 4 task 9")
+r = 2
+A = 12.5678
+print ("Radius is:" , r)
+print ("Area is: ", round(A,2)
+
+print ("Week 4 task 10")
+p = 17
+q = 18
+
+a = p//10+p%10
+b = p%2 + q % 2
+c = (p+q)//2
+d =(p+q/2.0)
+
+print(a,"<-a ",b,"<-b",c,"<-c",d,"<-d")
+
+
